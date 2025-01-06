4401688961f8d22fe80bec78da7c15d148cdcffd
Eithan Smith <yahia.smeism@sappro-bilisim.com>
2025-01-06 19:34:58 +0300

    enhance input search styles

--- lib/core/widgets/inputs/standard_search_input.dart
+++ lib/core/widgets/inputs/standard_search_input.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'standard_input.dart';
 
-class StandardSearchInput extends StatelessWidget {
+class StandardSearchInput extends StatefulWidget {
   const StandardSearchInput({
     super.key,
     required this.onSearch,
@@ -12,28 +12,84 @@ class StandardSearchInput extends StatelessWidget {
     this.controller,
     this.onFieldSubmitted,
   });
+
   final Function(BuildContext context, String keyword) onSearch;
   final bool delayOnChangeCallback;
   final FocusNode? focusNode;
   final TextEditingController? controller;
   final Function(String)? onFieldSubmitted;
+
   @override
+  State<StandardSearchInput> createState() => _StandardSearchInputState();
+}
+
+class _StandardSearchInputState extends State<StandardSearchInput> {
+  late TextEditingController _controller;
+  late FocusNode _focusNode;
+  bool _showClearIcon = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = widget.controller ?? TextEditingController();
+    _focusNode = widget.focusNode ?? FocusNode();
+
+    // Listen for text changes to toggle the clear icon
+    _controller.addListener(_handleTextChange);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_handleTextChange);
+    if (widget.controller == null) {
+      _controller.dispose();
+    }
+    if (widget.focusNode == null) {
+      _focusNode.dispose();
+    }
+    super.dispose();
+  }
+
+  void _handleTextChange() {
+    setState(() {
+      _showClearIcon = _controller.text.isNotEmpty;
+    });
+  }
+
+  void _clearText() {
+    _controller.clear();
+    _focusNode.unfocus();
+    setState(() {
+      _showClearIcon = false;
+    });
+    widget.onSearch(context, '');
+  }
+
   @override
   Widget build(BuildContext context) {
     return StandardInput(
       
       withBorder: false,
       fillColor: AppColors.scaffoldBackgroundColor,
-      onFieldSubmitted: onFieldSubmitted,
+      onFieldSubmitted: widget.onFieldSubmitted,
       iconPrefix: const Icon(Icons.search),
-      focusNode: focusNode,
-      controller: controller,
-      delayOnChangeCallback: delayOnChangeCallback,
+      focusNode: _focusNode,
+      controller: _controller,
+      delayOnChangeCallback: widget.delayOnChangeCallback,
       maxHeight: 100,
       hintText: 'Search',
       onChanged: (keyword) {
-        onSearch(context, keyword);
+        widget.onSearch(context, keyword);
       },
+      iconSuffix: _showClearIcon
+          ? InkWell(
+              onTap: _clearText,
+              child: const Icon(
+                Icons.clear,
+                color: AppColors.iconColor,
+              ),
+            )
+          : null,
     );
   }
 }
